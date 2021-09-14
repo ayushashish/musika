361c37b9eaf87b0b82c44131507d4903c463ae07
Ayush Ashish <ayushashish0911@gmail.com>
2021-09-14 11:44:27 +0530

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Musika</h1>
+      <h2>Upload your music files here!</h2>
+      <p>Supports mp3 files</p>
+    </div>
+  );
+};
+
+export default Title;
